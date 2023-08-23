c219a9006309cba4aca3085ed82daae212cf510e
Ahmad <ahmad.magdy@ambassadors.com>
2023-08-23 21:40:11 +0200

    initialized project and tailwind configurationt and created basic file structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+	return (
+		<main className="relative">
+			nav
+			<section className="xl:padding-1 wide:padding-r padding-b "> hero</section>
+			<section className="padding"> populafr products</section>
+			<section className="padding"> Super quality</section>
+			<section className="padding-x padding-y"> services</section>
+			<section className="padding"> special offer</section>
+			<section className="padding bg-pale-blue"> customer reviews</section>
+			<section className="padding-x sm:py-32 py-16 w-full"> subscribe</section>
+			<section className="padding-x padding-t pb-8 bg-black ">footer</section>
+		</main>
+	);
+};
+
+export default App;
